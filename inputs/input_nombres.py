6d1420b2ae3e9a53826c7c7e66bfcbbde5eeb16c
Julian Safadi <julianmatiassafadi@hotmail.com>
2023-11-29 15:20:16 -0300

    inputs

---
+++ inputs/input_nombres.py
@@ -0,0 +1,5 @@
+nombre = input('che maestro, dame tu nombre: ')
+
+# mostrarlo
+print(f'el nombre es {nombre}') # Por mas que le pasemos un numero, siempre es un STRING
+
---
+++ inputs/input_numeros.py
@@ -0,0 +1,8 @@
+numero = input('dame un numero, capitan: ')
+
+resultado = numero * 2 # '66'
+resultadoNum = int(numero) * 2 # int duplicado
+resultadoNum = float(numero) * 2 # int duplicado
+
+print(resultado)
+print(resultadoNum)
